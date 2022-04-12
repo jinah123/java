816d4c571e81560537a015f2e04f37bf1082d4c5
jinah123 <90678289+jinah123@users.noreply.github.com>
2022-04-12 22:59:03 +0900

    Assignment_01

---
+++ Question_01.java
@@ -0,0 +1,17 @@
+package assignment_01;
+
+public class Question_01 {
+
+	public static void main(String[] args) {
+		int x = 90;
+		int y = 80;
+		int z= 100;
+		int sum = (x+y+z);
+		int avg = sum/3;
+		
+		System.out.println("kor "+x+"\nmat "+y+"\neng "+z+"\nsum "+sum+"\navg "+avg); 
+		
+
+	}
+
+}
---
+++ Question_02.java
@@ -0,0 +1,13 @@
+package assignment_01;
+
+public class Question_02 {
+
+	public static void main(String[] args) {
+		System.out.println("TTTTTTTTTT");
+	    System.out.println("TTTTTTTTTT");
+		System.out.println("    TT    ");
+		System.out.println("    TT    ");	     	
+		System.out.println("    TT    ");
+	}
+
+}
